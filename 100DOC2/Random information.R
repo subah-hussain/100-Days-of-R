7a6cccbdc3ec6b326b080a88b0235f2920b172e9
subah_hussain <subahhussain1821@gmail.com>
2023-07-26 13:15:39 +0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   100DOC 1.Rmd
    	modified:   100DOC2/100DOC 2.Rmd
    	new file:   100DOC2/100DOC-2.html
    	new file:   100DOC2/Random information.R

---
+++ 100DOC2/Random information.R
@@ -0,0 +1,15 @@
+letters[1]
+month.abb
+pi
+month.name
+
+log(12, 2) #natural log
+log10(2)
+exp(1)
+
+getwd() #which directory
+dir() #list files
+setwd()#set directory
+ls() #all of the variables in R
+
+save.image() #saves all data in R env
